90f15eb1696fce3cec2efac63712f57763e677e9
Angelina <angelinarwhite@gmail.com>
2022-06-12 21:06:01 -0400

    commit tree d89af655cb57ea49537f67c8f187f252ea62520e

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "F", "Brown", "Pretty chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Brown", "Pretty chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, "F", "Black", "Pretty chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "F", "Black", "Pretty chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Brown", "Pretty chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "Grizzly", "Pretty chill", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "F", "Brown", "Pretty chill", 1);
